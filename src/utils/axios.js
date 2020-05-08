628ef33d3b266cc23dee468be2b7904035b9addc
raychua <raychua@users.noreply.github.com>
2020-05-08 10:55:31 +0800

    change timeout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BACKENDURL,
   //baseURL: "http://localhost:4000",
-  timeout: 1000,
+  timeout: 4000,
 });
 
 instance.defaults.headers.post["Content-Type"] = "application/json";
